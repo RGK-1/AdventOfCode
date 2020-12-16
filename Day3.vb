62a0d86ff029f422739121f1e982e9b0a520863d
Ryan Kelly <ryakel@ashcombe.surrey.sch.uk>
2020-12-16 11:28:51 +0000

    16/12/2020 2.0

--- Day3.vb
+++ Day3.vb
@@ -5,17 +5,18 @@ Module Day3
     Sub Main()
         
         Using sr As StreamReader = New StreamReader("inputday3.txt")
-            Dim Line As Integer
+            Dim Line As String
 
             Dim Array1 As ArrayList = New ArrayList
             Dim Charnumb As Integer = 0
-            Dim ch1 As Integer = 0 
-            Line = sr.ReadLine()
+            'Dim ch1 As Integer = 0 
+
             Dim Down As Integer = 0
             Dim Across As Integer =0
             Dim Tree As Integer = 0
-            Dim Lines As Integer = 0
-           
+
+            'Dim Lines As Integer = 0
+            Line = sr.ReadLine()
             While Line <> Nothing
                 Console.WriteLine(Line)
                 Array1.Add(Line)
@@ -24,18 +25,20 @@ Module Day3
 
 
             End While
-            While Lines <> 30 
-                If Across > 30 Then 
+            While Down <> 323
+                If Across > 31 Then
                     Across = Across - 30
                 End If
                 Dim ch1 As Char = Array1(Down).Chars(Across)
                 If ch1 = ("#") Then
                     Tree += 1
                 End If
-                Lines += 1
+                Across += 3
+                Down += 1
             End While
             Console.WriteLine(Tree)
-                       
+            Console.ReadLine()
+
         End Using
     End Sub
 End Module
